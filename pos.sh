bc826e2b5707d62b18d9f283a4afb6a7775f8046
sudoers <ashleyy99@proton.me>
2024-05-14 00:29:36 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+export BUILD_USERNAME=hefatss
+export BUILD_HOSTNAME=arch
+export USE_CCACHE=1
+
 rm -rf .repo/local_manifests/
 #repo init rom
 repo init -u https://github.com/hefatss/PixelOS-Manifest.git -b fourteen-Bengal_515 --git-lfs
