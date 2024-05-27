75a360844afa59ec6d74ece72bc416036a034140
fàts <ashleyy99@proton.me>
2024-05-27 17:17:36 +0700

    Update evox.sh

--- evox.sh
+++ evox.sh
@@ -8,7 +8,6 @@ rm -rf .repo/local_manifests/
 repo init -u https://github.com/hefatss/manifest_evox -b udc-bengal-5.15 --git-lfs
 #Local manifests
 git clone https://github.com/hefatss/local_manifests -b evox .repo/local_manifests
-git clone git@github.com:hefatss/vendor_sign-priv.git -b 14 vendor/evolution-priv
 
 #build
 /opt/crave/resync.sh
