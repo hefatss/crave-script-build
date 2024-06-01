a52a20f9b0370dff59db6fde580405ecb8785601
fàts <ashleyy99@proton.me>
2024-06-02 00:28:02 +0700

    Update Evox > PixelOS

--- evox.sh
+++ evox.sh
@@ -5,15 +5,16 @@ export BUILD_HOSTNAME=ubuntu
 
 rm -rf .repo/local_manifests/
 #repo init rom
-repo init -u https://github.com/hefatss/manifest_evox -b bengal_515 --git-lfs
+repo init -u https://github.com/hefatss/PixelOS-Manifest -b bengal_515 --git-lfs
 #Local manifests
-git clone https://github.com/hefatss/local_manifests -b evox .repo/local_manifests
+git clone https://github.com/hefatss/local_manifests -b pos .repo/local_manifests
 
 #build
 /opt/crave/resync.sh
-git clone https://github.com/hefatss/vendor_sign-priv -b evox vendor/evolution-priv
+rm -rf vendor/aosp/signing/keys
+git clone https://github.com/hefatss/vendor_sign-priv -b pos vendor/aosp/signing/keys
 . build/envsetup.sh
-lunch lineage_topaz-userdebug
+lunch aosp_topaz-ap1a-userdebug
 export INLINE_KERNEL_BUILDING=true
 export ALLOW_MISSING_DEPENDENCIES=true
-m evolution
+make bacon
