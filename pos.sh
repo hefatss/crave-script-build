aff3b9ef9fbcd8bc38ac7c844e06de3d7cf12c63
fàts <ashleyy99@proton.me>
2024-06-02 18:32:55 +0700

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -11,10 +11,11 @@ git clone https://github.com/hefatss/local_manifests -b pos-topaz .repo/local_ma
 
 #build
 /opt/crave/resync.sh
-rm -rf vendor/aosp/signing/keys
-git clone https://github.com/hefatss/vendor_sign-priv -b pos vendor/aosp/signing/keys
+rm -rf vendor/extra
+git clone https://github.com/shravansayz/vendor_extra -b master vendor/extra
 . build/envsetup.sh
 lunch aosp_topaz-ap1a-userdebug
 export INLINE_KERNEL_BUILDING=true
 export ALLOW_MISSING_DEPENDENCIES=true
+m installclean
 make bacon
